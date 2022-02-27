6d5ed820d33eb8c79c9a30ab6e37d75dd80d484c
Mohamed Es-saadani <essaadani80@gmail.com>
2022-02-27 18:39:46 +0100

    initial commit

---
+++ src/components/MoviesList.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class MoviesList extends Component {
+  render() {
+    return (
+      <div className="movies-list">
+        <ul>
+          <li>Interstellar</li>
+          <li>Taken</li>
+          <li>The Matrix</li>
+          <li>Jhon Wick</li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default MoviesList;
