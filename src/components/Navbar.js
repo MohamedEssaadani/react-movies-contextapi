be80179e19d6970d3bc5c4a91d42fe1d1afdb18d
Mohamed Es-saadani <essaadani80@gmail.com>
2022-03-01 11:24:27 +0100

    accessing context using useContext

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,50 @@
-import React, { Component } from "react";
+import React, { Component, useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
 
-class Navbar extends Component {
-  // inject the shared state to this.context property
-  static contextType = ThemeContext;
-  render() {
-    // destructuring context
-    const { isLightTheme, light, dark } = this.context;
+// class Navbar extends Component {
+//   // inject the shared state to this.context property
+//   // static contextType = ThemeContext;
 
-    // theme to use
-    const theme = isLightTheme ? light : dark;
+//   render() {
+//     // destructuring context
+//     const { isLightTheme, light, dark } = this.context;
 
-    return (
-      <nav style={{ backgroundColor: theme.bg, color: theme.syntax }}>
-        <h1>Movies App</h1>
-        <ul>
-          <li>Home</li>
-          <li>Contact</li>
-          <li>About</li>
-        </ul>
-      </nav>
-    );
-  }
-}
+//     // theme to use
+//     const theme = isLightTheme ? light : dark;
+
+//     return (
+//       <nav style={{ backgroundColor: theme.bg, color: theme.syntax }}>
+//         <h1>Movies App</h1>
+//         <ul>
+//           <li>Home</li>
+//           <li>Contact</li>
+//           <li>About</li>
+//         </ul>
+//       </nav>
+//     );
+//   }
+// }
+
+export const Navbar = () => {
+  // get context
+  const context = useContext(ThemeContext);
+
+  // destructuring context
+  const { isLightTheme, light, dark } = context;
+
+  // theme to use
+  const theme = isLightTheme ? light : dark;
+
+  return (
+    <nav style={{ backgroundColor: theme.bg, color: theme.syntax }}>
+      <h1>Movies App</h1>
+      <ul>
+        <li>Home</li>
+        <li>Contact</li>
+        <li>About</li>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
